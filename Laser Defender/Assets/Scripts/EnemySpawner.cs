2f16f20f52807c450d3baf3e8507b29b043c5574
Cristian Ciocoi <cristian.c49575757@yahoo.com>
2020-09-23 14:02:48 +0300

    LaserDefenderVersion1.1

---
+++ Laser Defender/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] List<WaveConfig> waveConfig;
+    [SerializeField] int startingWave = 0;
+    [SerializeField] bool looping = false;
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        do
+        {
+            yield return StartCoroutine(SpawnAllWaves());
+        }
+        while (looping);
+        
+
+    }
+    private IEnumerator SpawnAllWaves()
+    {
+        for (int i = startingWave; i < waveConfig.Count; i++)
+        {
+            var currentWave = waveConfig[i];
+            yield return StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+        }
+    }
+
+    private IEnumerator SpawnAllEnemiesInWave(WaveConfig currentWave)
+    {
+        for(int i = 0; i < currentWave.GetNumberOfEnemies(); i++)
+        {
+            var newEnemy = Instantiate(currentWave.GetEnemyPrefab(), currentWave.GetWaypoints()[0].transform.position, Quaternion.identity);
+            newEnemy.GetComponent<EnemyPathing>().SetWaveConfig(currentWave);
+            yield return new WaitForSeconds(currentWave.GetTimeBetweenSpawns());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
